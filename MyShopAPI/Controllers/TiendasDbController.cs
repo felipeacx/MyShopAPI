acffe9d34ae9539497f1084cdccb4246e24a4c23
Felipe Acosta <juanfe1190@hotmail.com>
2020-08-02 19:11:54 -0500

    Creación del Controlador API.

--- MyShopAPI/Controllers/TiendasDbController.cs
+++ MyShopAPI/Controllers/TiendasDbController.cs
@@ -12,20 +12,6 @@ namespace MyShopAPI.Controllers
 {
     public class TiendasDbController : Controller
     {
-        [ApiController]
-        [Route("[controller]")]
-        [HttpGet]
-        public IEnumerable<WeatherForecast> Get()
-        {
-            var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
-            })
-            .ToArray();
-        }
         private readonly MyShopDbContext _context;
 
         public TiendasDbController(MyShopDbContext context)
